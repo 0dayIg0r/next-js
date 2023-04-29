447a30a8339176fd88273586f3538dc09a9f4542
unknown <igormedeiros.f@gmail.com>
2023-04-28 21:58:07 -0300

    listando, deletando task e função de compartilhamento.

--- tasks/src/pages/dashboard/index.tsx
+++ tasks/src/pages/dashboard/index.tsx
@@ -1,5 +1,5 @@
 import { GetServerSideProps } from "next";
-import { ChangeEvent, FormEvent, useState } from "react";
+import { ChangeEvent, FormEvent, useState, useEffect } from "react";
 import styles from "./styles.module.css";
 import Head from "next/head";
 
@@ -9,20 +9,104 @@ import TextArea from "@/components/textArea";
 import { FiShare2 } from "react-icons/fi";
 import { FaTrash } from "react-icons/fa";
 
-export default function Dashboard() {
+import { db } from "../../services/firebaseConnection";
+
+import {
+  addDoc,
+  collection,
+  query,
+  orderBy,
+  where,
+  onSnapshot,
+  doc,
+  deleteDoc,
+} from "firebase/firestore";
+
+import Link from "next/link";
+
+interface HomeProps {
+  user: {
+    email: string;
+  };
+}
+
+interface TaskProps {
+  id: string;
+  created: Date;
+  public: boolean;
+  task: string;
+  user: string;
+}
+
+export default function Dashboard({ user }: HomeProps) {
   const [input, setInput] = useState("");
   const [publicTask, setPublicTask] = useState(false);
+  const [tasks, setTasks] = useState<TaskProps[]>([]);
+
+  useEffect(() => {
+    async function loadTasks() {
+      const taskRef = collection(db, "tasks");
+      const q = query(
+        taskRef,
+        orderBy("created", "desc"),
+        // when using where in firebase create an index on bda, create an index desc and asc of every element in where and repite in inverse asc and desc.
+        where("user", "==", user?.email)
+      );
+      // real time
+      onSnapshot(q, (snapshot) => {
+        let list = [] as TaskProps[];
+
+        snapshot.forEach((doc) => {
+          list.push({
+            id: doc.id,
+            task: doc.data().task,
+            created: doc.data().created,
+            user: doc.data().user,
+            public: doc.data().public,
+          });
+        });
+        setTasks(list);
+      });
+    }
+
+    loadTasks();
+  }, [user?.email]);
 
   function handleChangePublic(e: ChangeEvent<HTMLInputElement>) {
     setPublicTask(e.target.checked);
   }
 
-  function handleRegisterTask(e: FormEvent) {
-    e.preventDefault();
+  async function handleRegisterTask(event: FormEvent) {
+    event.preventDefault();
 
     if (input === "") return;
-    console.log(input)
-    alert('ok')
+
+    try {
+      await addDoc(collection(db, "tasks"), {
+        task: input,
+        created: new Date(),
+        user: user?.email,
+        public: publicTask,
+      });
+
+      setInput("");
+      setPublicTask(false);
+    } catch (e: any) {
+      console.log(e.message);
+    }
+  }
+
+  async function handleShare(id: string) {
+    // auto ctrl c
+    await navigator.clipboard.writeText(
+      `${process.env.NEXT_PUBLIC_URL}/task/${id}`
+    );
+  }
+
+  async function handleDeleteTask(id: string) {
+    const docRef = doc(db, "tasks", id);
+
+    await deleteDoc(docRef);
   }
 
   return (
@@ -54,10 +138,11 @@ export default function Dashboard() {
               />
               <label>Deixar tarefa publica?</label>
             </div>
-            <button 
-            className={styles.button} 
-            onClick={handleRegisterTask}
-            type="submit">
+            <button
+              className={styles.button}
+              onClick={handleRegisterTask}
+              type="submit"
+            >
               Adicionar
             </button>
           </div>
@@ -65,22 +150,38 @@ export default function Dashboard() {
         <section className={styles.taskContainer}>
           <h1>Minhas tarefas</h1>
 
-          <article className={styles.task}>
-            <div className={styles.tagContainer}>
-              <label className={styles.tag}> Publico</label>
-              <button className={styles.shareButton}>
-                <FiShare2 size={22} color="#3183ff" />
-              </button>
-            </div>
+          {tasks.map((item) => (
+            <article key={item.id} className={styles.task}>
+              {item.public && (
+                <div className={styles.tagContainer}>
+                  <label className={styles.tag}> Publico</label>
+                  <button
+                    className={styles.shareButton}
+                    onClick={() => handleShare(item.id)}
+                  >
+                    <FiShare2 size={22} color="#3183ff" />
+                  </button>
+                </div>
+              )}
 
-            <div className={styles.taskContent}>
-              <p>Minha primeira tarefa de exemplo</p>
+              <div className={styles.taskContent}>
+                {item.public ? (
+                  <Link href={`/task/${item.id}`}>
+                    <p>{item.task}</p>
+                  </Link>
+                ) : (
+                  <p>{item.task}</p>
+                )}
 
-              <button className={styles.buttonTrash}>
-                <FaTrash size={24} color="#ea3140" />
-              </button>
-            </div>
-          </article>
+                <button
+                  className={styles.buttonTrash}
+                  onClick={() => handleDeleteTask(item.id)}
+                >
+                  <FaTrash size={24} color="#ea3140" />
+                </button>
+              </div>
+            </article>
+          ))}
         </section>
       </main>
     </div>
@@ -99,6 +200,11 @@ export const getServerSideProps: GetServerSideProps = async ({ req }) => {
     };
   }
   return {
-    props: {},
+    props: {
+      user: {
+        // return email of user
+        email: session?.user.email,
+      },
+    },
   };
 };
